415365082e82c0ab9df790d36b5c8e62410aa740
kay <schmitteckertkay@freenet.de>
2018-10-21 01:12:43 +0200

    final rnn

--- rnn.py
+++ rnn.py
@@ -1,4 +1,5 @@
 import torch
+import os
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
@@ -6,17 +7,16 @@ import npdata
 from torch.autograd import Variable
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pack_sequence, pad_sequence
 
-#data = npdata.data
-data = torch.load('traindata.pt')
-#print(len(data))
-#m, n = data.shape
+# READ DATA AND DEFINE SOME SIZES
+training_data = torch.load('traindata.pt')
 input_size = 3
 hidden_size = 10
-n_batches = len(data)
+n_batches = len(training_data)
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
+        # RNN with LSTM and Linear activation function for regression
         self.lstm = nn.LSTM(input_size, hidden_size, 2)
         self.linear = nn.Linear(hidden_size, 1) # 1 output for regression
 
@@ -27,46 +27,52 @@ class Model(nn.Module):
         return class_predictions
 
 
-model = Model()
+# INIT MODEL FROM SCRATCH OR EXISTING
+net_available = False
+model = None
+if (os.path.isfile("model.pt")):
+    model = torch.load("model.pt")
+    net_available = True
+else:
+    model = Model()
 
+# Hyperparams
 optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.8)
 criterion = nn.MSELoss()
 
-
+# just for sorting
 def getLen(item):
     return len(item)
 
-sorted_data = sorted(data, key=getLen, reverse=True)
+# PREPARE DATA FOR TRAINING
+sorted_data = sorted(training_data, key=getLen, reverse=True)
 prepared_data = [[seq[1:4] for seq in x] for x in sorted_data]
 packed_data = pack_sequence([torch.tensor(x) for x in prepared_data])
 
 prepared_targets = [[seq[4:5] for seq in x] for x in sorted_data]
-packed_targets = pack_sequence([torch.tensor(x) for x in prepared_data])
 max_size = len(sorted_data[0])
 
-for _ in range(20):
+batches, dimensions = pad_packed_sequence(packed_data)
 
-    #for batch in data:
-     #   np_batch = np.array(batch)
-      #  input_data = np_batch[:, [1, 2, 3]]
+# TRAINING
+for i in range(len(sorted_data)):
+    for _ in range(20):
 
-        batches, dimensions = pad_packed_sequence(packed_data)
-        targets, dimensions_target = pad_packed_sequence(packed_targets)
+        # INPUT DATA FOR NEURONS
+        batch = batches[:, i:i+1]
+        input_seq = Variable(batch).float()
+        target = Variable(torch.tensor([prepared_targets[i][0]])).float()
 
-        for i, batch in enumerate(batches):
+        # FORWARD PASS
+        model.train()
+        output = model(input_seq)
+        loss = criterion(output, target.float())
 
+        # BACKPROPAGATION AND UPDATE WEIGHTS
+        optimizer.zero_grad()
+        loss.backward()
+        print("prediction: ", output.item(), "; target: ", target.item(), "; loss: ", loss.item())
+        optimizer.step()
 
-            #input_data = batches.reshape(max_size, n_batches, input_size)
-            input_seq = Variable(batch).float()
-
-            target = Variable(targets[i]).float()
-
-            model.train()
-            output = model(input_seq)
-            loss = criterion(output, target.float())
-
-
-            optimizer.zero_grad()
-            loss.backward()
-            print("prediction: ", output.item(), "; target: ", target.item(), "; loss: ", loss.item())
-            optimizer.step()
+        #save model after every update
+        torch.save(model, open('model.pt', 'wb'))
